28f1ecf442724db4674d99a270761a48ab41436a
gaurang-tcs <98448274+gaurang-tcs@users.noreply.github.com>
2023-11-27 17:59:45 +0530

    commit

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,19 @@
-const path = require('path')
+const path = require('path');
+
 const express = require('express');
-const adminData = require('./admin')
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {prods: products, docTitle: 'Shop', path:'/'})
-})
+router.get('/', shopController.getIndex);
 
-module.exports = router
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
