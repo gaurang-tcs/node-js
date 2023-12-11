13d9dae71fc2ee149f66c785b7d83f9b15ba15d5
gaurang-tcs <98448274+gaurang-tcs@users.noreply.github.com>
2023-12-11 09:44:12 +0530

    updates

--- models/product.js
+++ models/product.js
@@ -1,30 +1,25 @@
-const Cart = require('./cart');
-const db = require('../util/database')
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize = require('../util/database');
+
+module.exports = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
-
-  save() {
-    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    )
-  }
-
-  static deleteById(id) {
-
-  }
-
-  static fetchAll(cb) {
-    return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id, cb) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+});
